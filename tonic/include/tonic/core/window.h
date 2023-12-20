ee23eb9fce285b4473a8e883c3eba87422c12e69
Pseurae <adhith.chand@gmail.com>
2023-12-20 13:12:09 +0530

    Add setters for Window callbacks

--- tonic/include/tonic/core/window.h
+++ tonic/include/tonic/core/window.h
@@ -29,13 +29,19 @@ struct WindowProperties final
     tonic::graphics::TextureFilter min_filter, mag_filter;
 };
 
-template<typename... Args>
-using Event = void(*)(Args...);
-
 // TODO: Add event callback setters.
 class Window final
 {
 public:
+    template<typename... Args>
+    using Event = void(*)(Args...);
+
+    using QuitCallback = Event<>;
+    using KeyCallback = Event<input::Key, input::Actions, input::Mods>;
+    using DropCallback = Event<const std::vector<std::string> &>;
+    using CursorPosCallback = Event<const glm::vec2 &>;
+    using WindowResizeCallback = Event<const glm::vec2 &>;
+
     [[nodiscard]] bool GetKey(input::Key key) const;
     [[nodiscard]] bool GetMouseButton(input::MouseButton key) const;
     [[nodiscard]] glm::vec2 GetMousePosition() const;
@@ -45,7 +51,11 @@ public:
     [[nodiscard]] inline const auto &GetSize() const { return m_Size; }
     [[nodiscard]] inline const auto &GetFramebufferSize() const { return m_FramebufferSize; }
 
-    friend class tonic::Engine;
+    inline void SetQuitCallback(const QuitCallback &f) { m_QuitCallback = f; }
+    inline void SetKeyCallback(const KeyCallback &f) { m_KeyCallback = f; }
+    inline void SetDropCallback(const DropCallback &f) { m_DropCallback = f; }
+    inline void SetCursorPosCallback(const CursorPosCallback &f) { m_CursorPosCallback = f; }
+    inline void SetWindowResizeCallback(const WindowResizeCallback &f) { m_WindowResizeCallback = f; }
 
 private:
     bool Create(const WindowProperties &props);
@@ -60,15 +70,16 @@ private:
 
     void SetAllWindowCallbacks();
 
-    Event<> m_QuitCallback = nullptr;
-    Event<input::Key, input::Actions, input::Mods> m_KeyCallback = nullptr;
-    Event<const std::vector<std::string> &> m_DropCallback = nullptr;
-    Event<const glm::vec2 &> m_CursorPosCallback = nullptr;
-    Event<const glm::vec2 &> m_WindowResizeCallback = nullptr;
+    QuitCallback m_QuitCallback = nullptr;
+    KeyCallback m_KeyCallback = nullptr;
+    DropCallback m_DropCallback = nullptr;
+    CursorPosCallback m_CursorPosCallback = nullptr;
+    WindowResizeCallback m_WindowResizeCallback = nullptr;
 
     WindowProperties m_WindowProps;
     GLFWwindow *m_Window;
 
     glm::ivec2 m_Size, m_FramebufferSize;
+    friend class tonic::Engine;
 };
 }
